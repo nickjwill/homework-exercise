de9466ae25b9abdc87d65f32adc4c0b4abce4021
Nicholas  Williams <nickwilliams@Nicholass-MacBook-Pro.local>
2017-10-22 18:38:59 -0600

    Built pages via controller generator

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def authors
+  end
+
+  def titles
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/authors'
+
+  get 'pages/titles'
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
